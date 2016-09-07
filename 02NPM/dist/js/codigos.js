96af8c62a5e70031b20047698b2109f8086ee85c
Jonathan MirCha <jonmircha@gmail.com>
2016-09-07 17:02:42 -0500

    Clase 5 Curso Node.js desde cero de Escuela Digital

--- 02NPM/dist/js/codigos.js
+++ 02NPM/dist/js/codigos.js
@@ -1,6 +1,32 @@
 'use strict';
 
-$(function () {
-	var nombre = 'Jonathan';
-	alert('Hola ' + nombre);
-});
+/*$(() => {
+	let nombre= 'Jonathan';
+	alert(`Hola ${nombre}`);
+});*/
+
+(function (d, w, $, WOW) {
+	'use strict';
+
+	var wow = new WOW();
+
+	$(d).ready(function () {
+		$(".owl-carousel").owlCarousel({
+			items: 3,
+			loop: true,
+			autoplay: true,
+			autoplayTimeout: 1000,
+			autoplayHoverPause: true
+		});
+
+		$('.nav').find('a').click(function (e) {
+			var idEnlace = $(this).attr('href');
+
+			$('html, body').animate({
+				scrollTop: $(idEnlace).offset().top
+			}, 1000);
+		});
+	});
+
+	wow.init();
+})(document, window, jQuery, WOW);
