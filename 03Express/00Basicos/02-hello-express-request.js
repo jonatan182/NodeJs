01928ebc002e8b457686d3076a168c1e7601b2d4
Jonathan MirCha <jonmircha@gmail.com>
2016-09-09 21:13:47 -0500

    Clase 6 Curso Node.js desde cero de Escuela Digital

---
+++ 03Express/00Basicos/02-hello-express-request.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express'),
+	app = express();
+
+app
+	.get( '/', (req, res) => res.end('<h1>Hola Mundo desde Express.js</h1>') )
+	//http://localhost:3000/user/19-jonathan-32
+	.get('/user/:id-:name-:age', (req, res) => {
+		res.end(`
+			<h1>
+				${req.params.name}, bienvenid@ a Express :) tu id es <b>${req.params.id}</b> y tienes ${req.params.age} a&ntilde;os
+			</h1>
+		`);
+	})
+	.get('/search', (req, res) => {
+		res.end(`
+			<h1>
+				Bienvenido a Express, los resultados de tu b&uacute;squeda son:
+				<mark>${req.query.s}</mark>
+			</h1>
+		`)
+	})
+	.listen( 3000, () => console.log('Iniciando Express en el puerto 3000') );
