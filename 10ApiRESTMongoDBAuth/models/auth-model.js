9d40edcffa8d94cd7fe01d9ebb9fea47018c75da
Jonathan MirCha <jonmircha@gmail.com>
2016-09-26 13:49:27 -0500

    Clase 21 Curso Node.js desde cero de Escuela Digital

--- 10ApiRESTMongoDBAuth/models/auth-model.js
+++ 10ApiRESTMongoDBAuth/models/auth-model.js
@@ -3,11 +3,22 @@ const conn = require('./auth-schema');
 
 class AuthModel {
 	getUser(user, cb) {
-
+		conn
+			.findOne({
+				username : user.username,
+				password : user.password
+			})
+			.exec((err, docs) => {
+				if (err) throw err;
+				cb(docs);
+			});
 	}
 
 	setUser(user, cb) {
-
+		conn.create(user, (err) => {
+			if(err) throw err;
+			cb();
+		});
 	}
 }
 
